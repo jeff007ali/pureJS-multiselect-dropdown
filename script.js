b61e1f9c5d9b477219868c4485efd57c008fbae9
jeff007ali <jagharia@gmail.com>
2020-07-06 00:43:17 +0530

    Alternative code for bootstrap multiselect dropdown

---
+++ script.js
@@ -0,0 +1,74 @@
+options = ["English", "Hindi", "Spanish", "French"];
+ddListId = "langDDlist";
+
+templates = {
+    button: '<button type="button" class="multiselect dropdown-toggle btn btn-default" data-toggle="dropdown" title="None selected" aria-expanded="false"><span class="multiselect-selected-text">None selected</span> <b class="caret"></b></button>',
+    ul: '<ul class="multiselect-container dropdown-menu"></ul>',
+    li: '<li><a tabindex="0"><label></label></a></li>'
+}
+
+ddListObj = document.getElementById(ddListId);
+
+
+function addOption(value) {
+    // console.log(value);
+    
+    ulObj = document.querySelector("#" + ddListId + " .dropdown-menu");
+    ulObj.innerHTML += templates.li;
+
+    liObj = document.querySelectorAll("#" + ddListId + " .dropdown-menu label");
+    liObj[liObj.length - 1].innerHTML = '<input type="checkbox" value=' + value + ' onchange="selectOption(this)"> ' + value;
+}
+
+function buildDropdown() {
+    ddListObj.innerHTML = templates.button + templates.ul;
+    options.forEach(addOption);
+
+    var ddToggle = document.querySelector("#" + ddListId + " .dropdown-toggle");
+    ddToggle.addEventListener("click", function() {
+        if (ddListObj.classList.contains("open")) {
+            ddListObj.classList.remove("open");
+            console.log(this);
+            this.setAttribute('aria-expanded', 'false');
+        }
+        else {
+            ddListObj.classList.add("open");
+            this.setAttribute('aria-expanded', 'true');
+        }
+    });
+
+    // var ddMenuCheckBoxes = document.querySelectorAll("#" + ddListId + " .dropdown-menu input[type=checkbox]");
+    // console.log(ddMenuCheckBoxes);
+    // for (ddMenuCheckBox in ddMenuCheckBoxes) {
+    //     ddMenuCheckBox.addEventListener("change", function() {
+    //         if (this.checked){
+    //             console.log(this.value);
+    //         } else {
+    //             console.log(this.value);
+    //         }
+    //     });
+    // }
+    
+}
+
+function selectOption(e) {
+    var ddToggleSpan = document.querySelector("#" + ddListId + " .dropdown-toggle span");
+    
+    if (e.checked) {
+        if (ddToggleSpan.innerText == "None selected") {
+            ddToggleSpan.innerText = e.value;
+        } else {
+            ddToggleSpan.innerText += ", " + e.value;
+        }
+        
+    } else {
+        var optionList = ddToggleSpan.innerText.split(", ");
+        filteredOptions = optionList.filter(option => option !== e.value);
+        ddToggleSpan.innerText = filteredOptions.join(", ");
+    }
+    var ddToggle = document.querySelector("#" + ddListId + " .dropdown-toggle");
+    ddToggle.title = ddToggleSpan.innerText;
+}
+
+
+window.addEventListener("load", buildDropdown);
